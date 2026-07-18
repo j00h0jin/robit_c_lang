0f82761db539f8d4fadc63e50a36df8d54ce28e9
j00h0jin <hojin7710@daum.net>
2026-07-18 21:48:02 +0900

    ✅ day 2 task 2

--- day2/task_2.c
+++ day2/task_2.c
@@ -1,5 +1,4 @@
 // 선택 정렬
-// 정렬 된 부분 / 안 된 부분 구분한 뒤
 // 정렬 안 된 부분에서 최댓값 지정
 
 #include <stdio.h>
@@ -10,6 +9,7 @@ void main()
         0,
     };
     int temp, max;
+
     printf("입력\n");
     for (int i = 0; i < 8; i++)
     {
@@ -17,19 +17,24 @@ void main()
         scanf(" %d", &arr[i]);
     }
 
+    printf("\n");
+
+    // 처음에 입력 값 출력
     for (int i = 0; i < 8; i++)
     {
         printf("%2d ", arr[i]);
     }
-    printf("\n");
+    printf("\n\n");
 
     int last = 7;
-    int k = 0;
 
     while (last > 0)
     {
+        // 항상 배열의 첫번째 수를 기준으로
         temp = arr[0];
+        int k = 0;
 
+        // 최댓값 찾기 (k는 최댓값의 인덱스 번호임)
         for (int i = 1; i <= last; i++)
         {
             if (temp < arr[i])
@@ -39,25 +44,28 @@ void main()
             }
         }
 
-        printf("%d\n", k);
-
         max = temp;
-        printf("%d ", max);
+        // if (max = last) => skip
+        // last는 정렬이 안된 수 중에 가장 마지막 index
+        if (max == arr[last])
+        {
+            last -= 1;
+            continue;
+        }
         temp = arr[last];
-        printf("%d ", temp);
         arr[last] = max;
-        printf("%d ", arr[last]);
         arr[k] = temp;
-        printf("%d\n", arr[k]);
-
-        // if k = last => skip
 
         for (int i = 0; i < 8; i++)
         {
             printf("%2d ", arr[i]);
         }
-        printf("\n");
+        printf("\n\n");
 
         last -= 1;
     }
+
+    printf("출력\n");
+    for (int i = 0; i < 8; i++)
+        printf("%d : %2d\n", i + 1, arr[i]);
 }
