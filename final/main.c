0f540091845f814599279f264c459110f60247a9
j00h0jin <hojin7710@daum.net>
2026-07-25 15:55:57 +0900

    🚧

--- final/main.c
+++ final/main.c
@@ -1,63 +1,173 @@
+/*
+gcc main.c -o app.exe -lgdi32 -luser32
+app.exe
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#define width 100
-#define height 100
+#include <windows.h>
 
-unsigned char h[54];
-unsigned char R[width][height], G[width][height], B[width][height];
+#define MAX_SWORDS 30
 
-int main(int argc, char *argv[])
+// 1. 각 검의 이미지 정보를 담을 구조체 정의
+typedef struct
 {
-    int i, j;
-    FILE *in = fopen("image.bmp", "rb");
-    FILE *out = fopen("trans.bmp", "wb");
-    // *in은 image.bmp를 가리키는 주기억 장치 내 포인터, FILE 변수이다.
-    // rb는 read binary, wb는 write binary의 준말이다.
+    BITMAPINFOHEADER bih;
+    unsigned char *pixelData;
+    int isLoaded;
+} BMPImage;
 
-    if (in == NULL)
-    { // "=" 만약 image가 없다면
-        printf("파일이 없습니다.");
+// 30개의 검 이미지를 저장할 배열
+BMPImage g_swords[MAX_SWORDS];
+int g_currentSwordIndex = 0; // 현재 화면에 띄울 검 번호 (0 ~ 29)
+
+// BMP 파일 로드 함수 (구조체로 저장하도록 변경)
+int load_bmp(const char *filename, BMPImage *outImg)
+{
+    FILE *fp = fopen(filename, "rb");
+    if (!fp)
+    {
+        printf("[오류] 파일 열기 실패: %s\n", filename);
+        outImg->isLoaded = 0;
         return 0;
     }
 
-    for (i = 0; i < 54; i++)
-        h[i] = getc(in);
-    // h[i]가 in의 처음부터 54번째까지의 원소를 포함하도록
-    for (i = 0; i < width; i++)
-        for (j = 0; j < height; j++)
-        { // window에서 BGR 순으로 저장
-            B[i][j] = getc(in);
-            G[i][j] = getc(in);
-            R[i][j] = getc(in);
-        }
-    for (i = 0; i < width; i++)
-        for (j = 0; j < height; j++)
+    BITMAPFILEHEADER bfh;
+    fread(&bfh, sizeof(BITMAPFILEHEADER), 1, fp);
+    fread(&outImg->bih, sizeof(BITMAPINFOHEADER), 1, fp);
+
+    if (bfh.bfType != 0x4D42)
+    { // 'BM' 마크 확인
+        printf("[오류] 올바른 BMP 포맷이 아닙니다: %s\n", filename);
+        fclose(fp);
+        outImg->isLoaded = 0;
+        return 0;
+    }
+
+    int imgWidth = outImg->bih.biWidth;
+    int imgHeight = abs(outImg->bih.biHeight);
+    int bpp = outImg->bih.biBitCount;
+
+    int rowSize = ((imgWidth * bpp + 31) / 32) * 4;
+    int imageSize = rowSize * imgHeight;
+
+    outImg->pixelData = (unsigned char *)malloc(imageSize);
+    if (!outImg->pixelData)
+    {
+        fclose(fp);
+        outImg->isLoaded = 0;
+        return 0;
+    }
+
+    fseek(fp, bfh.bfOffBits, SEEK_SET);
+    fread(outImg->pixelData, 1, imageSize, fp);
+    fclose(fp);
+
+    // ★ [수정] 32bit BMP 투명 배경 보정
+    if (bpp == 32)
+    {
+        for (int i = 0; i < imageSize; i += 4)
         {
-            if (B[i][j] == 0 && G[i][j] == 0 && R[i][j] == 0)
-            { // 픽셀이 검정이면
-                B[i][j] = 255;
-                G[i][j] = 255;
-                R[i][j] = 255;
-            }
-            else
+            unsigned char alpha = outImg->pixelData[i + 3];
+
+            // 알파(투명도)가 0인 투명 영역은 완전한 불투명 흰색(255)으로 변경
+            if (alpha == 0)
             {
-                B[i][j] = 0;
-                G[i][j] = 0;
-                R[i][j] = 0;
+                outImg->pixelData[i] = 255;     // Blue
+                outImg->pixelData[i + 1] = 255; // Green
+                outImg->pixelData[i + 2] = 255; // Red
+                outImg->pixelData[i + 3] = 255; // ★ Alpha도 255(불투명)로 설정해야 보임!
             }
+        }
+    }
+
+    outImg->isLoaded = 1;
+    printf("[성공] 로드 완료: %s\n", filename);
+    return 1;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        // 현재 선택된 검 이미지 가져오기
+        BMPImage *curImg = &g_swords[g_currentSwordIndex];
+
+        if (curImg->isLoaded && curImg->pixelData != NULL)
+        {
+            BITMAPINFO bmi;
+            ZeroMemory(&bmi, sizeof(BITMAPINFO));
+
+            bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+            bmi.bmiHeader.biWidth = curImg->bih.biWidth;
+            bmi.bmiHeader.biHeight = curImg->bih.biHeight;
+            bmi.bmiHeader.biPlanes = 1;
+            bmi.bmiHeader.biBitCount = curImg->bih.biBitCount;
+            bmi.bmiHeader.biCompression = BI_RGB;
+
+            SetDIBitsToDevice(hdc, 10, 10, curImg->bih.biWidth, abs(curImg->bih.biHeight), 0, 0, 0,
+                              abs(curImg->bih.biHeight), curImg->pixelData, &bmi, DIB_RGB_COLORS);
         }
 
-    for (i = 0; i < 54; i++)
-        fputc(h[i], out);
-    for (i = 0; i < width; i++)
-        for (j = 0; j < height; j++)
+        EndPaint(hwnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        // 프로그램 종료 시 모든 메모리 해제
+        for (int i = 0; i < MAX_SWORDS; i++)
         {
-            fputc(B[i][j], out);
-            fputc(G[i][j], out);
-            fputc(R[i][j], out);
-            // 처리된 것들을 모두 보조디스크 내 trans 파일에 입력
+            if (g_swords[i].pixelData)
+            {
+                free(g_swords[i].pixelData);
+            }
         }
-    fclose(in);
-    fclose(out);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
     return 0;
+}
+
+int main()
+{
+    // 30개 검 이미지 반복문으로 로드
+    char filePath[256];
+    for (int i = 0; i < MAX_SWORDS; i++)
+    {
+        sprintf(filePath, "asset/sword_%d.bmp", i);
+        load_bmp(filePath, &g_swords[i]);
+    }
+
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    const char g_szClassName[] = "MyBMPWindowClass";
+
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszClassName = g_szClassName;
+
+    RegisterClassEx(&wc);
+
+    HWND hwnd = CreateWindowExA(WS_EX_CLIENTEDGE, g_szClassName, "검 강화하기 - 테스트", WS_OVERLAPPEDWINDOW,
+                                CW_USEDEFAULT, CW_USEDEFAULT, 900, 1000, NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+    MSG Msg;
+    while (GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+
+    return Msg.wParam;
 }
--- test.c
+++ test.c
@@ -1,6 +1,142 @@
-#include <stdio.h>
+/*
+gcc main.c -o app.exe -lgdi32
+app.exe
+*/
 
-void main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+BITMAPFILEHEADER bfh;
+BITMAPINFOHEADER bih;
+unsigned char *pixelData = NULL;
+
+int load_bmp(const char *filename)
 {
-    printf("test");
+    FILE *fp = fopen(filename, "rb");
+    if (!fp)
+        return 0;
+
+    fread(&bfh, sizeof(BITMAPFILEHEADER), 1, fp);
+    fread(&bih, sizeof(BITMAPINFOHEADER), 1, fp);
+
+    if (bfh.bfType != 0x4D42)
+    { // 'BM' 마크 확인
+        fclose(fp);
+        return 0;
+    }
+
+    int imgWidth = bih.biWidth;
+    int imgHeight = abs(bih.biHeight);
+    int bpp = bih.biBitCount;
+
+    int rowSize = ((imgWidth * bpp + 31) / 32) * 4;
+    int imageSize = rowSize * imgHeight;
+
+    pixelData = (unsigned char *)malloc(imageSize);
+    if (!pixelData)
+    {
+        fclose(fp);
+        return 0;
+    }
+
+    fseek(fp, bfh.bfOffBits, SEEK_SET);
+    fread(pixelData, 1, imageSize, fp);
+    fclose(fp);
+
+    // ★ [핵심] 32bit BMP의 투명 배경(Alpha = 0)을 흰색(255)으로 보정 ★
+    if (bpp == 32)
+    {
+        for (int i = 0; i < imageSize; i += 4)
+        {
+            unsigned char alpha = pixelData[i + 3];
+
+            // 알파(투명도)가 0인 투명 배경은 흰색으로 칠함
+            if (alpha == 0)
+            {
+                pixelData[i] = 255;     // Blue
+                pixelData[i + 1] = 255; // Green
+                pixelData[i + 2] = 255; // Red
+            }
+        }
+    }
+
+    return 1;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        if (pixelData != NULL)
+        {
+            BITMAPINFO bmi;
+            ZeroMemory(&bmi, sizeof(BITMAPINFO));
+
+            bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+            bmi.bmiHeader.biWidth = bih.biWidth;
+            bmi.bmiHeader.biHeight = bih.biHeight;
+            bmi.bmiHeader.biPlanes = 1;
+            bmi.bmiHeader.biBitCount = bih.biBitCount;
+            bmi.bmiHeader.biCompression = BI_RGB;
+
+            SetDIBitsToDevice(hdc, 10, 10, bih.biWidth, abs(bih.biHeight), 0, 0, 0, abs(bih.biHeight), pixelData, &bmi,
+                              DIB_RGB_COLORS);
+        }
+
+        EndPaint(hwnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        if (pixelData)
+            free(pixelData);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+int main()
+{
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+
+    if (!load_bmp("asset/test.bmp"))
+    {
+        printf("[오류] test.bmp 읽기 실패!\n");
+        system("pause");
+        return 0;
+    }
+
+    const char g_szClassName[] = "MyBMPWindowClass";
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszClassName = g_szClassName;
+
+    RegisterClassEx(&wc);
+
+    // 이미지 해상도(846x896)에 맞춰 창 크기도 넉넉하게 설정 (900x1000)
+    HWND hwnd = CreateWindowExA(WS_EX_CLIENTEDGE, g_szClassName, "검 강화하기", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                                CW_USEDEFAULT, 900, 1000, NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hwnd, SW_SHOW);
+    UpdateWindow(hwnd);
+
+    MSG Msg;
+    while (GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+
+    return Msg.wParam;
 }
